3e90323c257fb7b02d75da5aa3a5256d0c30a84c
James Montemagno <james.montemagno@gmail.com>
2022-05-12 15:52:39 -0700

    Add starting for exercise3

---
+++ exercise3/TipCalculator/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TipCalculator;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ exercise3/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace TipCalculator;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        billInput.TextChanged += (s, e) => CalculateTip(false, false);
+        roundDown.Clicked += (s, e) => CalculateTip(false, true);
+        roundUp.Clicked += (s, e) => CalculateTip(true, false);
+
+        tipPercentSlider.ValueChanged += (s, e) =>
+        {
+            double pct = Math.Round(e.NewValue);
+            tipPercent.Text = pct + "%";
+            CalculateTip(false, false);
+        };
+    }
+
+    void CalculateTip(bool roundUp, bool roundDown)
+    {
+        double t;
+        if (Double.TryParse(billInput.Text, out t) && t > 0)
+        {
+            double pct = Math.Round(tipPercentSlider.Value);
+            double tip = Math.Round(t * (pct / 100.0), 2);
+
+            double final = t + tip;
+
+            if (roundUp)
+            {
+                final = Math.Ceiling(final);
+                tip = final - t;
+            }
+            else if (roundDown)
+            {
+                final = Math.Floor(final);
+                tip = final - t;
+            }
+
+            tipOutput.Text = tip.ToString("C");
+            totalOutput.Text = final.ToString("C");
+        }
+    }
+
+    void OnNormalTip(object sender, EventArgs e) { tipPercentSlider.Value = 15; }
+    void OnGenerousTip(object sender, EventArgs e) { tipPercentSlider.Value = 20; }
+}
